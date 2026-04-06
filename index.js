f3cb273f0f1d73d94b46da9f12d1ffa9b1e67bb6
Prince <mutukuprince6@gmail.com>
2026-04-06 20:29:15 +0300

    added a file

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-
-
+//creating a login attempt
+function createLogin() {
+    let attempt = 2;
+    let login = (pin) => {
+        if (attempt === 0) {
+            console.log("access denied") 
+        }
+        if (pin === userInfo.pin){
+            console.log("access granted")  
+        }
+        else {
+            attempt--;
+            console.log(`incorrect pin. You have ${attempt} attemp left`);
+        }
+    }
+    return login;
+};
+//creating user info
+let userInfo = {
+    name: "Martin Maxwell",
+    pin: "250250"
+};
+let loginattempt = createLogin(userInfo.pin);
+//loginTrials
+//firstTrial
+loginattempt("254254"); //Output: incorrect pin.
+loginattempt("250250"); //Output:access granted.
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
